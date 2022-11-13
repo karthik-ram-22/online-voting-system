9a1fad4ed085a4f9461531a450e98e2fa75f9432
karthik-ram-22 <114208155+karthik-ram-22@users.noreply.github.com>
2022-11-13 23:25:06 +0530

    Add files via upload

---
+++ static/election.js
@@ -0,0 +1,22 @@
+
+function elections(){
+    
+    var one = document.getElementById("one");
+    var two = document.getElementById("two");
+    var three = document.getElementById("three");
+    var four = document.getElementById("four");
+    var five = document.getElementById("five");
+    var six = document.getElementById("six");
+
+    /*var userdisplay = document.getElementById("userdisplay");
+    userdisplay.innerHTML=localStorage.getItem("user");
+    signinError.style.display="block";
+    */
+    one.innerHTML="HOOGRAJULI ASSAM TEA CO LTD----- (12-oct-2022 to 18-oct-2022)";
+    two.innerHTML="TAMILNAD MERCANTILE BANK LIMITED#FORMERLY NADAR BANK LIMITED----- (12-oct-2022 to 18-oct-2022)";
+    three.innerHTML="MOONGIPA SECURITIES LIMITED#FORMERLY MOONGIPA SECURITIES PRIVATE LIMITED----- (12-oct-2022 to 18-oct-2022)";
+    four.innerHTML="INDIAN ORTHOPAEDIC ASSOCIATION-DEMO----- (12-oct-2022 to 18-oct-2022)";
+    five.innerHTML="ESTANCIA FLAT OWNERS ASSOCIATION----- (12-oct-2022 to 18-oct-2022)";
+    six.innerHTML="SECMARK CONSULTANCY LIMITED#FORMERLY SECMARK CONSULTANCY PRIVATE LIMITED----- (12-oct-2022 to 18-oct-2022)";
+
+}
